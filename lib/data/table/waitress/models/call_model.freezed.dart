503b31284d8cb6ed3c835997d4eb9e522805f2c5
IslomjonCoder <106533499+IslomjonCoder@users.noreply.github.com>
2024-02-27 01:16:57 +0500

    create order

---
+++ lib/data/table/waitress/models/call_model.freezed.dart
@@ -0,0 +1,205 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'call_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+CallModel _$CallModelFromJson(Map<String, dynamic> json) {
+  return _CallModel.fromJson(json);
+}
+
+/// @nodoc
+mixin _$CallModel {
+  @JsonKey(name: "_id")
+  String get id => throw _privateConstructorUsedError;
+  @JsonKey(name: "name")
+  String get name => throw _privateConstructorUsedError;
+  @JsonKey(name: "id")
+  String get callModelId => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $CallModelCopyWith<CallModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $CallModelCopyWith<$Res> {
+  factory $CallModelCopyWith(CallModel value, $Res Function(CallModel) then) =
+      _$CallModelCopyWithImpl<$Res, CallModel>;
+  @useResult
+  $Res call(
+      {@JsonKey(name: "_id") String id,
+      @JsonKey(name: "name") String name,
+      @JsonKey(name: "id") String callModelId});
+}
+
+/// @nodoc
+class _$CallModelCopyWithImpl<$Res, $Val extends CallModel>
+    implements $CallModelCopyWith<$Res> {
+  _$CallModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+    Object? callModelId = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      callModelId: null == callModelId
+          ? _value.callModelId
+          : callModelId // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$CallModelImplCopyWith<$Res>
+    implements $CallModelCopyWith<$Res> {
+  factory _$$CallModelImplCopyWith(
+          _$CallModelImpl value, $Res Function(_$CallModelImpl) then) =
+      __$$CallModelImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {@JsonKey(name: "_id") String id,
+      @JsonKey(name: "name") String name,
+      @JsonKey(name: "id") String callModelId});
+}
+
+/// @nodoc
+class __$$CallModelImplCopyWithImpl<$Res>
+    extends _$CallModelCopyWithImpl<$Res, _$CallModelImpl>
+    implements _$$CallModelImplCopyWith<$Res> {
+  __$$CallModelImplCopyWithImpl(
+      _$CallModelImpl _value, $Res Function(_$CallModelImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+    Object? callModelId = null,
+  }) {
+    return _then(_$CallModelImpl(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      callModelId: null == callModelId
+          ? _value.callModelId
+          : callModelId // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$CallModelImpl implements _CallModel {
+  const _$CallModelImpl(
+      {@JsonKey(name: "_id") required this.id,
+      @JsonKey(name: "name") required this.name,
+      @JsonKey(name: "id") required this.callModelId});
+
+  factory _$CallModelImpl.fromJson(Map<String, dynamic> json) =>
+      _$$CallModelImplFromJson(json);
+
+  @override
+  @JsonKey(name: "_id")
+  final String id;
+  @override
+  @JsonKey(name: "name")
+  final String name;
+  @override
+  @JsonKey(name: "id")
+  final String callModelId;
+
+  @override
+  String toString() {
+    return 'CallModel(id: $id, name: $name, callModelId: $callModelId)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$CallModelImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.callModelId, callModelId) ||
+                other.callModelId == callModelId));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(runtimeType, id, name, callModelId);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$CallModelImplCopyWith<_$CallModelImpl> get copyWith =>
+      __$$CallModelImplCopyWithImpl<_$CallModelImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$CallModelImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _CallModel implements CallModel {
+  const factory _CallModel(
+          {@JsonKey(name: "_id") required final String id,
+          @JsonKey(name: "name") required final String name,
+          @JsonKey(name: "id") required final String callModelId}) =
+      _$CallModelImpl;
+
+  factory _CallModel.fromJson(Map<String, dynamic> json) =
+      _$CallModelImpl.fromJson;
+
+  @override
+  @JsonKey(name: "_id")
+  String get id;
+  @override
+  @JsonKey(name: "name")
+  String get name;
+  @override
+  @JsonKey(name: "id")
+  String get callModelId;
+  @override
+  @JsonKey(ignore: true)
+  _$$CallModelImplCopyWith<_$CallModelImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
